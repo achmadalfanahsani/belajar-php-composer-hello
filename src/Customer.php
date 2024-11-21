20493b0167c70e82f20c71b0dc25d12b966cea88
Achmad Alfan Ahsani <achmadalfanahsani@gmail.com>
2024-11-22 01:43:04 +0700

    Add default value

--- src/Customer.php
+++ src/Customer.php
@@ -8,7 +8,7 @@ class Customer
     private string $name
   ) {}
 
-  public function sayHello(string $name): string
+  public function sayHello(string $name = "Guest"): string
   {
     return "Hello $name, my name is $this->name";
   }
